562e1fcdeca3011c4ad6328daa0c3bc171f43c02
Pixez <djmcmaggi@gmail.com>
2025-09-29 11:16:15 +0200

    Still broken CLI tooling but now almost supported for piping stuff.

---
+++ MapGenTool/GeneratorCommand.cs
@@ -0,0 +1,32 @@
+﻿using System.CommandLine;
+
+namespace MapGenTool;
+
+internal interface IGeneratorCommand
+{
+    public Command Command { get; set; }
+    public bool IsInputSupported(Type type);
+}
+internal class GeneratorCommand<TOut>(Command command) :IGeneratorCommand
+{
+    public Command Command { get; set; } = command;
+
+    public bool IsInputSupported(Type type) => false;
+}
+internal class GeneratorCommand<TIn,TOut>(Command command) : IGeneratorCommand
+{
+    public Command Command { get; set; } = command;
+
+    public bool IsInputSupported(Type type) => type == typeof(TIn);
+}
+internal class GeneratorCommand<TIn1,TIn2, TOut>(Command command) : IGeneratorCommand
+{
+    public Command Command { get; set; } = command;
+
+    public bool IsInputSupported(Type type)
+    {
+        return
+            type == typeof(TIn1) ||
+            type == typeof(TIn2);
+    }
+}
--- MapGenTool/Program.cs
+++ MapGenTool/Program.cs
@@ -7,83 +7,133 @@ using MapGenTool.Generators.NoiseGenerators;
 using MapGenTool.Generators.RoomGenerators;
 using System.Diagnostics;
 
-Option<int> widthOption = new(
-    "--width", "-w")
+Option<int> widthOption = new("--width", "-w")
 {
     Description = "Width of the map",
-    Required = true,
-    DefaultValueFactory = _ => 120
+    Required = true
 };
-
-Option<int> heightOption = new(
-    "--height", "-h")
+Option<int> heightOption = new("--height", "-h")
 {
     Description = "Height of the map",
-    Required = true,
-    DefaultValueFactory = _ => 30
+    Required = true
 };
-
-Option<float> scaleOption = new(
-    "--scale", "-S")
+Option<float> scaleOption = new("--scale", "-S")
 {
     Description = "Scale factor for the image",
     Required = false,
-    DefaultValueFactory = _ => 1f,
+    DefaultValueFactory = parseResult => 1f
 };
-
-Option<int> seedOption = new(
-    "--seed", "-s")
+Option<int> seedOption = new("--seed", "-s")
 {
     Description = "Seed for generation",
     Required = false,
-    DefaultValueFactory = _ => Random.Shared.Next()
+    DefaultValueFactory = parseResult => Random.Shared.Next()
 };
-
-Argument<string> pathArgument = new(
-    "path")
+Argument<FileInfo> pathArgument = new("path")
 {
     Description = "Output path for the image"
 };
 
-RootCommand rootCommand = new("MapGenTool CLI")
-{
-    widthOption,
-    heightOption,
-    scaleOption,
-    seedOption,
-    pathArgument
-};
+RootCommand rootCommand = new("MapGenTool");
+rootCommand.Options.Add(widthOption);
+rootCommand.Options.Add(heightOption);
+rootCommand.Options.Add(scaleOption);
+rootCommand.Options.Add(seedOption);
+rootCommand.Arguments.Add(pathArgument);
 
-rootCommand.SetAction(parseResult =>
-{
-    int width = parseResult.GetValue(widthOption);
-    int height= parseResult.GetValue(heightOption);
-
-    float scale= parseResult.GetValue(scaleOption);
-
-    int seed= parseResult.GetValue(seedOption);
-    string path= parseResult.GetValue(pathArgument);
-
-    VoronoiNoiseGenerator voronoi = new(10);
-    SobelEdgeDetection sobel = new(voronoi.Generate);
-    ThresholdClamper clamper = new(sobel.Generate, 0.4f);
-
-    Console.WriteLine("Starting generation...");
-    var test = clamper.Generate(width, height, seed);
-
-    string fullPath = Path.GetFullPath(path + ".png");
-    MapDrawer.DrawBitMap(fullPath, test, scale, (int)scale);
-    Console.WriteLine($"Successfully created map at {fullPath}");
-
-    if (File.Exists(fullPath))
+var voronoi = new GeneratorCommand<byte>(new("voronoi", "Generates a voronoi noise texture. (byte[*.*])"));
+voronoi.Command.Options.Add(
+    new Option<int>("--grid-scale", "-G")
     {
-        Process.Start(new ProcessStartInfo(fullPath) { UseShellExecute = true });
-    }
-    else
+        Description = "The scale of the grid in which the seeds are placed."
+    });
+var sobel = new GeneratorCommand<byte, byte>(new("sobel", "Takes input and runs Sobel edge detection on it. (byte[*.*])"));
+var bsp = new GeneratorCommand<Tiles>(new("bsp", "Generates room and corridors in subdivided edges. (Tiles[*.*])"));
+bsp.Command.Options.Add(
+    new Option<int>("--particion-count, -P")
     {
-        Console.WriteLine("Failed to find the image file.");
+        Description = "How many partitions the image should be split into. The number of room equals 2 to the power of n.",
+        Required = true
+    });
+var clamper = new GeneratorCommand<byte, Tiles>(new("clamper", "Clamps given byte[*,*] into Tiles[*,*] walls and spaces. (Tiles[*.*])"));
+clamper.Command.Options.Add(
+    new Option<float>("--threshold", "-T")
+    {
+        Description = "The threshold at which to place spases insted of walls",
+        Required=true,
+        DefaultValueFactory = parseResult => 0.5f
+    });
+var conway = new GeneratorCommand<Tiles, Tiles>(new("conway", "Runs Conway's game of life on give Tiles[*,*] where spaces are alive cells. (Tiles[*.*])"));
+conway.Command.Options.Add(
+    new Option<int>("--iterations", "-I")
+    {
+        Description = "The number of iterations the simulation should run for before output.",
+        Required = true,
+    });
+//var drunkardsWalk = new GeneratorCommand<Tiles, Tiles>(new("drunkards", "Runs drunkards walk algorythm on given image. Drunkard can start on any space cells. (Tiles[*.*])"));
+var simpleNoise = new GeneratorCommand<Tiles>(new("simple-nosie", "Gets a simple randomized noise. (Tiles[*.*])"));
+
+IGeneratorCommand[] generatorCommands = [
+    voronoi,
+    sobel,
+    bsp,
+    clamper,
+    conway,
+    //drunkardsWalk,
+    simpleNoise
+];
+foreach (var generatorCommand in generatorCommands)
+{
+    rootCommand.Subcommands.Add(generatorCommand.Command);
+
+    var supportedCommands = generatorCommands
+        .Where(
+            c => c.IsInputSupported(
+                generatorCommand.GetType().GenericTypeArguments.Last()));
+    foreach (var c in supportedCommands)
+    {
+        generatorCommand.Command.Subcommands.Add(c);
     }
+}
 
-}, widthOption, heightOption, scaleOption, seedOption, pathArgument);
 
-return rootCommand.Parse(args).Invoke();
+ParseResult results = rootCommand.Parse(args);
+
+if (results.Errors.Any() || results.GetValue(pathArgument) is not FileInfo fileInfo)
+{
+    foreach (var error in results.Errors)
+        Console.Error.WriteLine(error.Message);
+
+    return 1;
+}
+
+int width = results.GetValue(widthOption);
+int height = results.GetValue(heightOption);
+
+float scale = results.GetValue(scaleOption);
+
+int seed = results.GetValue(seedOption);
+string path = Path.GetFullPath(fileInfo.FullName);
+
+List<>
+foreach (var generatorCommand in generatorCommands)
+{
+
+}
+
+Console.WriteLine("Starting generation...");
+//var test = clamper.Generate(width, height, seed);
+
+MapDrawer.DrawBitMap(path, test, scale, (int)scale);
+Console.WriteLine($"Successfully created map at {path}");
+
+if (File.Exists(path))
+{
+    Process.Start(new ProcessStartInfo(path) { UseShellExecute = true });
+}
+else
+{
+    Console.WriteLine("Failed to find the image file.");
+}
+
+return 0;
