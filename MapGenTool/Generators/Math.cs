084e707706510eb80eeb9b8aae4539a813b7202b
Jakab Magor <magor6666@gmail.com>
2026-03-30 13:36:52 +0200

    misc fix

--- MapGenTool/Generators/Math.cs
+++ MapGenTool/Generators/Math.cs
@@ -1,19 +1,19 @@
 ﻿namespace MapGenTool.Generators;
 public static partial class Misc {
-    public static byte[,] Multiply(int width, int height, (byte[,] a, byte[,] b) t) {
+    public static byte[,] Multiply(int width, int height, byte[,] a, byte[,] b) {
         byte[,] r = new byte[width, height];
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                r[x, y] = (byte)(t.a[x, y] * t.b[x, y] / 255);
+                r[x, y] = (byte)(a[x, y] * b[x, y] / 255);
             }
         }
         return r;
     }
-    public static byte[,] Add(int width, int height, (byte[,] a, byte[,] b) t) {
+    public static byte[,] Add(int width, int height, byte[,] a, byte[,] b) {
         byte[,] r = new byte[width, height];
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                int v = t.a[x, y] + t.b[x, y];
+                int v = a[x, y] + b[x, y];
                 if (v > 255) {
                     v = 255;
                 }
@@ -22,11 +22,11 @@ public static partial class Misc {
         }
         return r;
     }
-    public static byte[,] Subtract(int width, int height, (byte[,] a, byte[,] b) t) {
+    public static byte[,] Subtract(int width, int height, byte[,] a, byte[,] b) {
         byte[,] r = new byte[width, height];
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                int v = t.a[x, y] - t.b[x, y];
+                int v = b[x, y] - a[x, y];
                 if (v < 0) {
                     v = 0;
                 }
@@ -35,11 +35,11 @@ public static partial class Misc {
         }
         return r;
     }
-    public static byte[,] Divide(int width, int height, (byte[,] a, byte[,] b) t) {
+    public static byte[,] Divide(int width, int height, byte[,] a, byte[,] b) {
         byte[,] r = new byte[width, height];
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                r[x, y] = (byte)(t.a[x, y] / t.b[x, y]);
+                r[x, y] = (byte)(b[x, y] / a[x, y]);
             }
         }
         return r;
--- MapGenTool/Program.cs
+++ MapGenTool/Program.cs
@@ -273,7 +273,7 @@ try {
             Console.SetOut(TextWriter.Null);
         } else {
             Console.WriteLine();
-            Console.SetOut(new GeneratorConsoleWriter(stdOut, "\t\t"));
+            Console.SetOut(new GeneratorLoggerWriter(stdOut, "\t\t"));
         }
 
         Stopwatch sWatch = new();
@@ -348,16 +348,16 @@ try {
                         cullingTreshold: int.Parse(generatorArgs[0])));
                     break;
                 case "*":
-                    byteStack.Push(Misc.Multiply(width, height, pop2AndSwap(ref byteStack)));
+                    byteStack.Push(Misc.Multiply(width, height, byteStack.Pop(), byteStack.Pop()));
                     break;
                 case "/":
-                    byteStack.Push(Misc.Divide(width, height, pop2AndSwap(ref byteStack)));
+                    byteStack.Push(Misc.Divide(width, height, byteStack.Pop(), byteStack.Pop()));
                     break;
                 case "+":
-                    byteStack.Push(Misc.Add(width, height, pop2AndSwap(ref byteStack)));
+                    byteStack.Push(Misc.Add(width, height, byteStack.Pop(), byteStack.Pop()));
                     break;
                 case "-":
-                    byteStack.Push(Misc.Subtract(width, height, pop2AndSwap(ref byteStack)));
+                    byteStack.Push(Misc.Subtract(width, height, byteStack.Pop(), byteStack.Pop()));
                     break;
                 case "prefab-room":
                     tileStack.Push(Rooms.PrefabRooms(width, height, seed,
@@ -367,7 +367,6 @@ try {
             }
             sWatch.Stop();
         }
-
 #if !DEBUG
         catch (IndexOutOfRangeException) {
             throw new IndexOutOfRangeException($"Invalid pipeline. Not enough arguments provided to \"{name}\".");
@@ -405,12 +404,6 @@ try {
     }
 
     return (t, idx);
-}
-
-static (T, T) pop2AndSwap<T>(ref Stack<T> stack) {
-    var a = stack.Pop();
-    var b = stack.Pop();
-    return (b, a);
 }
 
 /// ----------------------------------------------
